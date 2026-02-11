0f1434325d30fc0ef47a97817c4cf48c924b25fc
njoki <njerinjoki22@gmail.com>
2026-02-11 12:22:10 +0300

    add: FAQ section with toggle

---
+++ src/config/faqs.ts
@@ -0,0 +1,32 @@
+export type FAQItem = {
+    question: string;
+    answer: string;
+};
+
+export const faqs: FAQItem[] = [
+    {
+        question: "What is Design Lab?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        question: "How does the membership work",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    },
+    {
+        question: "How fast are design requests delivered?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse."
+    },
+    {
+        question: "Can I cancel anytime?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Excepteur sint occaecat cupidatat non proident."
+    },
+    {
+        question: "Do you work with startups or enterprises?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque."
+    },
+    {
+        question: "What kind of design services do you offer?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit."
+    }
+
+]
